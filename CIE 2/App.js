42d38838c18f310f54564cba2629d668d967e365
VARSHAC06 <varshacbca24@rvu.edu.in>
2026-03-23 16:41:51 +0530

    Initialize React Native app with navigation setup

---
+++ CIE 2/App.js
@@ -0,0 +1,20 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './src/screens/HomeScreen';
+import DestinationScreen from './src/screens/DestinationScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Destination" component={DestinationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
